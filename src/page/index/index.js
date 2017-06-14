78d4037d52d07aba8079a564743714377a696006
midoDaddy <fisheeep@gmail.com>
2017-06-14 16:52:25 +0800

    util v2.0

--- src/page/index/index.js
+++ src/page/index/index.js
@@ -2,8 +2,12 @@
 * @Author: midoDaddy
 * @Date:   2017-06-09 12:28:17
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-06-14 16:12:15
+* @Last Modified time: 2017-06-14 16:51:14
 */
 require('./index.css');
 var _mm = require('util/mm.js');
-console.log(_mm.getUrlParam('test'));
+var html = '<div>{{first}}</div><div>{{second}}</div>';
+var data = {
+    first: 123,
+    second: 456
+};
--- src/util/mm.js
+++ src/util/mm.js
@@ -1,6 +1,7 @@
 var conf = {
     serverHost: ''
 };
+var Hogan = require('hogan');
 var _mm = {
     //网络请求
     request: function(param) {
@@ -39,9 +40,40 @@ var _mm = {
         var results = window.location.search.substring(1).match(reg);
         return results ? results[2] : null;
     },
+    //渲染html模板
+    renderHtml: function(htmlTemplate, data) {
+        var template = Hogan.compile(htmlTemplate),
+            result = template.render(data);
+        return result;
+    },  
+    //操作成功消息提示
+    successTips: function(msg) {
+        alert(msg || '操作成功！');
+    },
+    //操作失败消息提示
+    errorTips: function(msg) {
+        alert(msg || '出问题啦~');
+    },
+    //字段验证，支持非空、手机、邮箱的验证
+    validate: function(value, type) {
+        var value = $.trim(value);
+        if (type === 'require') {
+            return !!value;
+        }
+        if (type === 'phone') {
+            return /^1\d{10}$/.test(value);
+        }
+        if (type === 'email') {
+            return /^(\w)+(\.\w+)*@(\w)+((\.\w{2,3}){1,3})$/.test(value);
+        }
+    },
     //统一登录处理
     doLogin: function() {
         window.location.href = './login.html?redirect=' + encodeURIComponent(window.location.href)
+    },
+    //回到主页
+    goHome: function() {
+        window.location.href = './index.html';
     },
 };
 module.exports = _mm;
