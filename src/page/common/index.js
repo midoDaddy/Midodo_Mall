77d9fb67373051170b16ed801c206cbbda482418
midoDaddy <fisheeep@gmail.com>
2017-06-14 23:12:38 +0800

    add layout

--- src/page/common/index.js
+++ src/page/common/index.js
@@ -2,6 +2,6 @@
 * @Author: midoDaddy
 * @Date:   2017-06-09 14:59:49
 * @Last Modified by:   midoDaddy
-* @Last Modified time: 2017-06-12 17:11:37
+* @Last Modified time: 2017-06-14 17:14:40
 */
-console.log('i am global')
+require('./layout.css');
