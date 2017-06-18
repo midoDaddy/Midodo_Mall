0b5077c00cb40c5855e75af7ceec6c25fac51bac
midoDaddy <fisheeep@gmail.com>
2017-06-18 10:12:49 +0800

    add nav

---
+++ src/service/cart-service.js
@@ -0,0 +1,22 @@
+/*
+* @Author: midoDaddy
+* @Date:   2017-06-18 09:05:25
+* @Last Modified by:   midoDaddy
+* @Last Modified time: 2017-06-18 09:08:23
+*/
+
+'use strict';
+var _mm = require('util/mm.js');
+
+var _cart = {
+    //获取购物车数量
+    getCartCount: function(resolve, reject){
+        _mm.request({
+            url: _mm.getSververUrl('/cart/get_cart_product_count.do')，
+            success: resolve,
+            error: reject
+        });
+    }
+}
+
+module.exports = _cart;
---
+++ src/service/user-service.js
@@ -0,0 +1,32 @@
+/*
+* @Author: midoDaddy
+* @Date:   2017-06-18 08:47:33
+* @Last Modified by:   midoDaddy
+* @Last Modified time: 2017-06-18 09:01:11
+*/
+
+'use strict';
+var _mm = require('util/mm.js');
+
+var _user = {
+    //登出
+    logout: function(resolve, reject){
+        _mm.request({
+            url: _mm.getSververUrl('/user/logout.do')，
+            method: 'POST',
+            success: resolve,
+            error: reject
+        })
+    },
+    //检查登录状态
+    checkLogin: function(){
+        _mm.request({
+            url: _mm.getSververUrl('/user/get_user_info.do')，
+            method: 'POST',
+            success: resolve,
+            error: reject
+        })
+    }
+}
+
+module.exports = _user;
